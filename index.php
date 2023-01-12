44fc5daf4a6da68d097307804fbcbef1e809a434
Brandon Burgess <burgess.brandon@student.greenriver.edu>
2023-01-12 17:32:55 -0500

    added index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+//This is my controller for the pets project
+
+//turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//require the autoload file
+require_once('vendor/autoload.php');
+
+//create an instance of the Base class
+$f3 = Base::instance();
+//Java equivalent -> Base f3 = new Base();
+
+//Define a default route ("home page" for hello project
+$f3->route('GET /', function() {
+    echo '<h1>Pet Home</h1>';
+//    $view = new Template();
+//    echo $view->render('views/home.html');
+});
+
+//Run Fat Free
+$f3->run();
+//Java -> f3.run();
+
+
+
